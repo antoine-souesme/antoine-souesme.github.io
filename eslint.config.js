8da5d1915491c6a2414c98a826832c291773c051
Antoine Souesme <antoine@lapala.io>
2025-03-06 17:21:52 +0100

    eslint fonts and homepage

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,77 @@
-import js from '@eslint/js'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     { ignores: ['dist'] },
     {
-        extends: [js.configs.recommended, ...tseslint.configs.recommended],
-        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+        ],
+        files: ['**/*.{js,jsx,ts,tsx}'],
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
         },
         plugins: {
+            'react': react,
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
+
             'react-refresh/only-export-components': [
                 'warn',
                 { allowConstantExport: true },
             ],
+
+            "indent": [
+                "error",
+                4,
+                {
+                    "SwitchCase": 1,
+                },
+            ],
+            "semi": [
+                "error",
+                "always",
+            ],
+            "comma-dangle": [
+                "error",
+                {
+                    "arrays": "always-multiline",
+                    "objects": "always-multiline",
+                    "imports": "always-multiline",
+                    "exports": "always-multiline",
+                    "functions": "always-multiline",
+                },
+            ],
+
+            "react/jsx-max-props-per-line": [
+                "error",
+                {
+                    "maximum": 1,
+                },
+            ],
+            "react/jsx-curly-spacing": [
+                "error",
+                {
+                    "when": "always",
+                },
+            ],
+            'react/jsx-first-prop-new-line': [
+                'error',
+                'multiline',
+            ],
+            "react/jsx-closing-bracket-location": [
+                "error",
+                "line-aligned",
+            ],
         },
     },
-)
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import clsx from "clsx";
 
 function App() {
     return (
-        <div>
-            Hello World!
+        <div
+            id=''
+            className={ clsx([
+                'container',
+            ]) }
+        >
+
+            {/* HEADER */}
+            <header>
+                <div className="flex flex-col items-center justify-center pt-128 gap-16">
+                    <h1 className='font-playwrite text-accent text-4xl'>Antoine Souesme</h1>
+                    <p className="text-text text-lg">Site en cours de construction</p>
+                </div>
+            </header>
+
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
